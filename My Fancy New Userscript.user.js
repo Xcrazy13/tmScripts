1ded654d342c24d299efcd2ead4e9fa12408806a
Benjamin Hill <benjaminhill@HILLBENJAMIN.local>
2015-06-19 08:58:43 -0400

    a
    fdasfdsa

---
+++ My Fancy New Userscript.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         My Fancy New Userscript
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+https://raw.githubusercontent.com/Asexual/agar.io-hacks/master/Agar.io%20cheatycodes%20-%20hacks.user.js
+
